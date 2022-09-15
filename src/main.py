b21513682f6819d540d18d99dd43a56251e876e6
Maxim Kolomeychenko <maxim@supervise.ly>
2022-09-15 13:59:26 +0300

    ui -wip

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
 import os
 from dotenv import load_dotenv
 import supervisely as sly
-from supervisely.app.widgets import Card
+from supervisely.app.widgets import Card, Container, Video
 
 # for convenient debug, has no effect in production
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
-card = Card("Title", "Description")
+vid1 = 3267369
+vid2 = 3267370
+
+v1 = Video(vid1)
+v2 = Video(vid2)
+
+card1 = Card("Input video #1", "Select first video", content=v1)
+card2 = Card("Input video #2", "Select second video", content=v2)
+input_cards = Container(widgets=[card1, card2], direction="horizontal", gap=15)
+
+
+card = Card("Tagging", "Description")
+layout = Container(widgets=[input_cards, card], direction="vertical", gap=15)
+
 
 api = sly.Api()
-app = sly.Application(layout=card)
+app = sly.Application(layout=layout)
