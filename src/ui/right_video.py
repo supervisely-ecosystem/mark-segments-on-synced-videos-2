fa0cd210882c96dfac675efd7df6f7f068199272
Maxim Kolomeychenko <maxim@supervise.ly>
2022-09-18 13:37:27 +0300

    sync with left

--- src/ui/right_video.py
+++ src/ui/right_video.py
@@ -1,14 +1,24 @@
 import supervisely as sly
-from supervisely.app.widgets import Card, Video, VideoThumbnail, Container
+from supervisely.app.widgets import Card, Video, VideoThumbnail, Container, Button
+
+import src.ui.left_video as left_video
 
 preview = VideoThumbnail()
 preview.hide()
 
 player = Video()
+sync_btn = Button("Sync with left", button_type="text", icon="zmdi zmdi-time")
+
 card = Card(
     "📹 Input video #2",
     "Navigate and tag segments endings on this video",
-    content=Container([preview, player], direction="vertical"),
+    content=Container([preview, player, sync_btn], direction="vertical"),
     lock_message='Select video in table by clicking 👆 "SET RIGHT" button on step 3️⃣',
 )
 card.lock()
+
+
+@sync_btn.click
+def sync_with_left():
+    left_frame = left_video.player.get_current_frame()
+    player.set_current_frame(left_frame)
